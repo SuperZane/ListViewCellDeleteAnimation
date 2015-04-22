b35508470badc40c4a3a74ea0b724a59f2fc78c7
SuperZane <litzsearch@gmail.com>
2015-04-22 20:27:59 +0800

    Update MainActivity.java
    there is no need re-inflate the view after dismiss, u can re-show it by reset the view's layoutparams attribution

--- src/com/example/myanimtest/MainActivity.java
+++ src/com/example/myanimtest/MainActivity.java
@@ -26,136 +26,117 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	static final int ANIMATION_DURATION = 200;
-	private static List<MyCell> mAnimList = new ArrayList<MyCell>();
-	private MyAnimListAdapter mMyAnimListAdapter;
+    static final int ANIMATION_DURATION = 200;
+    private static List<MyCell> mAnimList = new ArrayList<MyCell>();
+    private MyAnimListAdapter mMyAnimListAdapter;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		for (int i=0;i<50;i++) {
-			MyCell cell = new MyCell();
-			cell.name = "Cell No."+Integer.toString(i);
-			mAnimList.add(cell);
-		}
+        for (int i=0;i<50;i++) {
+            MyCell cell = new MyCell();
+            cell.name = "Cell No."+Integer.toString(i);
+            mAnimList.add(cell);
+        }
 
-		mMyAnimListAdapter = new MyAnimListAdapter(this, R.layout.chain_cell, mAnimList);
-		ListView myListView = (ListView) findViewById(R.id.chainListView);
-		myListView.setAdapter(mMyAnimListAdapter);
-	}
+        mMyAnimListAdapter = new MyAnimListAdapter(this, R.layout.chain_cell, mAnimList);
+        ListView myListView = (ListView) findViewById(R.id.chainListView);
+        myListView.setAdapter(mMyAnimListAdapter);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
-	private void deleteCell(final View v, final int index) {
-		AnimationListener al = new AnimationListener() {
-			@Override
-			public void onAnimationEnd(Animation arg0) {
-				mAnimList.remove(index);
+    private void deleteCell(final View v, final int index) {
+        collapse(v);
+    }
 
-				ViewHolder vh = (ViewHolder)v.getTag();
-				vh.needInflate = true;
+    private void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
 
-				mMyAnimListAdapter.notifyDataSetChanged();
-			}
-			@Override public void onAnimationRepeat(Animation animation) {}
-			@Override public void onAnimationStart(Animation animation) {}
-		};
+        Animation anim = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime >= 1) {
+                    v.setVisibility(View.GONE);
+                }
+                else {
+                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
 
-		collapse(v, al);
-	}
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
 
-	private void collapse(final View v, AnimationListener al) {
-		final int initialHeight = v.getMeasuredHeight();
-
-		Animation anim = new Animation() {
-			@Override
-			protected void applyTransformation(float interpolatedTime, Transformation t) {
-				if (interpolatedTime == 1) {
-					v.setVisibility(View.GONE);
-				}
-				else {
-					v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
-					v.requestLayout();
-				}
-			}
-
-			@Override
-			public boolean willChangeBounds() {
-				return true;
-			}
-		};
-
-		if (al!=null) {
-			anim.setAnimationListener(al);
-		}
-		anim.setDuration(ANIMATION_DURATION);
-		v.startAnimation(anim);
-	}
+        anim.setDuration(ANIMATION_DURATION);
+        v.startAnimation(anim);
+    }
 
 
-	private class MyCell {
-		public String name;
-	}
+    private class MyCell {
+        public String name;
+    }
 
-	private class ViewHolder {
-		public boolean needInflate;
-		public TextView text;
-		ImageButton imageButton;
-	}
+    private class ViewHolder {
+        public TextView text;
+        ImageButton imageButton;
+    }
 
-	public class MyAnimListAdapter extends ArrayAdapter<MyCell> {
-		private LayoutInflater mInflater;
-		private int resId;
+    public class MyAnimListAdapter extends ArrayAdapter<MyCell> {
+        private LayoutInflater mInflater;
+        private int resId;
 
-		public MyAnimListAdapter(Context context, int textViewResourceId, List<MyCell> objects) {
-			super(context, textViewResourceId, objects);
-			this.resId = textViewResourceId;
-			this.mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		}
+        public MyAnimListAdapter(Context context, int textViewResourceId, List<MyCell> objects) {
+            super(context, textViewResourceId, objects);
+            this.resId = textViewResourceId;
+            this.mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
 
-		@Override
-		public View getView(final int position, View convertView, ViewGroup parent) {
-			final View view;
-			ViewHolder vh;
-			MyCell cell = (MyCell)getItem(position);
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            final View view;
+            ViewHolder vh;
+            MyCell cell = (MyCell)getItem(position);
 
-			if (convertView==null) {
-				view = mInflater.inflate(R.layout.chain_cell, parent, false);
-				setViewHolder(view);
-			}
-			else if (((ViewHolder)convertView.getTag()).needInflate) {
-				view = mInflater.inflate(R.layout.chain_cell, parent, false);
-				setViewHolder(view);
-			}
-			else {
-				view = convertView;
-			}
+            if (convertView==null) {
+                view = mInflater.inflate(R.layout.chain_cell, parent, false);
+                setViewHolder(view);
+            } else {
+                view = convertView;
+            }
 
-			vh = (ViewHolder)view.getTag();
-			vh.text.setText(cell.name);
-			vh.imageButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					deleteCell(view, position);
-				}
-			});
+            vh = (ViewHolder)view.getTag();
+            vh.text.setText(cell.name);
+            vh.imageButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteCell(view, position);
+                }
+            });
+            if (view.getVisibility() != View.VISIBLE) {
+                view.setVisibility(View.VISIBLE);
+                if (view.getLayoutParams() != null) {
+                    view.getLayoutParams().height = LayoutParams.MATCH_PARENT;
+                }
+            }
+            return view;
+        }
 
-			return view;
-		}
-
-		private void setViewHolder(View view) {
-			ViewHolder vh = new ViewHolder();
-			vh.text = (TextView)view.findViewById(R.id.cell_name_textview);
-			vh.imageButton = (ImageButton) view.findViewById(R.id.cell_trash_button);
-			vh.needInflate = false;
-			view.setTag(vh);
-		}
-	}
+        private void setViewHolder(View view) {
+            ViewHolder vh = new ViewHolder();
+            vh.text = (TextView)view.findViewById(R.id.cell_name_textview);
+            vh.imageButton = (ImageButton) view.findViewById(R.id.cell_trash_button);
+            view.setTag(vh);
+        }
+    }
 }
